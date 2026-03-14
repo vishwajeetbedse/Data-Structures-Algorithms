7888221cd452425e56e6576af7d18592909bc331
Vishwajeet Bedse <vishwajeetbedse@gmail.com>
2026-03-14 08:08:26 +0530

    Time: 7 ms (29.05%), Space: 15.4 MB (5.12%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        vector<string> o = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+        vector<string> t = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+        vector<string> h = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+        vector<string> th = {"","M","MM","MMM"};
+        return th[num/1000] + h[(num%1000)/100] + t[(num%100)/10] + o[num % 10];
+    }
+};
