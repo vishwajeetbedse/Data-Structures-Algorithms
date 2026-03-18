cd46f5fc06eaa406f1f392bb07589b7299616899
Vishwajeet Bedse <vishwajeetbedse@gmail.com>
2026-03-18 08:22:56 +0530

    Time: 0 ms (100%), Space: 9.1 MB (54.81%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int myAtoi(string s) {
+        int i = 0, sign = 1;
+        long res = 0; 
+        while (i < s.size() && s[i] == ' ') i++;
+        if (i == s.size()) return 0;
+        if (s[i] == '-') { sign = -1; i++; }
+        else if (s[i] == '+') i++;
+        while (i < s.size() && isdigit(s[i])) {
+            res = res * 10 + (s[i] - '0');
+            if (sign * res > INT_MAX) return INT_MAX;
+            if (sign * res < INT_MIN) return INT_MIN;
+            i++;
+        }
+        return (int)(sign * res);
+    }
+};
